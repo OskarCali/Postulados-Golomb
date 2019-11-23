24a4acc88ec4932c98848dce7165bdd64cdef79d
Oskar <oskarcali18@gmail.com>
2019-11-22 22:26:39 -0600

    Update view details
    Actualizacion de la vista detalles para que sean personalizadas

--- Postulados Golomb/Views/Details.Designer.cs
+++ Postulados Golomb/Views/Details.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formDetails));
             this.lblTitle = new System.Windows.Forms.Label();
             this.tabControl = new System.Windows.Forms.TabControl();
             this.SuspendLayout();
@@ -35,9 +36,10 @@
             // lblTitle
             // 
             this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("MS Reference Sans Serif", 13.8F, System.Drawing.FontStyle.Bold);
             this.lblTitle.Location = new System.Drawing.Point(0, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(800, 62);
+            this.lblTitle.Size = new System.Drawing.Size(1032, 62);
             this.lblTitle.TabIndex = 1;
             this.lblTitle.Text = "POSTULADOS DE GOLOMB";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -48,19 +50,22 @@
             this.tabControl.Location = new System.Drawing.Point(0, 62);
             this.tabControl.Name = "tabControl";
             this.tabControl.SelectedIndex = 0;
-            this.tabControl.Size = new System.Drawing.Size(800, 388);
+            this.tabControl.Size = new System.Drawing.Size(1032, 591);
             this.tabControl.TabIndex = 2;
             // 
             // formDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1032, 653);
             this.Controls.Add(this.tabControl);
             this.Controls.Add(this.lblTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(1050, 700);
             this.Name = "formDetails";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Details";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.formDetails_Load);
             this.ResumeLayout(false);
 
--- Postulados Golomb/Views/Details.cs
+++ Postulados Golomb/Views/Details.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 using Postulados_Golomb.Models;
@@ -15,11 +9,11 @@ namespace Postulados_Golomb.Views
 {
     public partial class formDetails : Form
     {
-        private List<int> _p1;
-        private List<List<Element>> _p2;
-        private List<double> _p3;
-        private int _bin;
-        private int _indice;
+        private readonly List<int> _p1;
+        private readonly List<List<Element>> _p2;
+        private readonly List<double> _p3;
+        private readonly int _bin;
+        private readonly int _indice;
 
         public formDetails(int indice, List<int> postulatesAnalisisP1)
         {
@@ -53,7 +47,7 @@ namespace Postulados_Golomb.Views
                 case 1:
                     chart = new Chart();
                     chart.ChartAreas.Add("Area");
-                    chart.ChartAreas["Area"].AxisX.IntervalAutoMode = IntervalAutoMode.VariableCount;
+                    //chart.ChartAreas["Area"].AxisX.IntervalAutoMode = IntervalAutoMode.VariableCount;
                     chart.Series.Add("Postulado1");
                     chart.Series["Postulado1"].ChartType = SeriesChartType.Column;
                     chart.Series["Postulado1"]["PointWidth"] = "0.5";
@@ -66,34 +60,30 @@ namespace Postulados_Golomb.Views
                             item = x
                         })
                         .ToList()
-                        .ForEach(obj =>
-                        {
-                            chart.Series["Postulado1"].Points.AddXY(obj.item, obj.count);
-                        });
+                        .ForEach(obj => { chart.Series["Postulado1"].Points.AddXY(obj.item, obj.count); });
 
                     tabControl.TabPages.Add("1", "TabPage1");
                     tabControl.TabPages["1"].Controls.Add(chart);
                     break;
                 case 2:
-                    for (int i = 1; i < _bin; i++)
+                    for (var i = 1; i < _bin; i++)
                     {
                         chart = new Chart();
                         chart.ChartAreas.Add("Area");
                         chart.ChartAreas["Area"].AxisX.IntervalAutoMode = IntervalAutoMode.VariableCount;
                         chart.Series.Add("Bin " + (i + 1));
                         chart.Series["Bin " + (i + 1)].ChartType = SeriesChartType.Column;
-                        chart.Series["Bin " + (i + 1)]["PointWidth"] = "0.5";
+                        //chart.Series["Bin " + (i + 1)]["PointWidth"] = "0.5";
                         chart.Series["Bin " + (i + 1)].IsValueShownAsLabel = true;
                         chart.Dock = DockStyle.Fill;
 
-                        foreach (var item in _p2[i-1])
+                        foreach (var item in _p2[i - 1])
                             chart.Series["Bin " + (i + 1)].Points.AddXY(item.Name, item.Coincidences);
-                        //item.ForEach(x => chart.Series["Bin " + (i + 1)].Points.AddXY(x.Name, x.Coincidences));
 
-
-                        tabControl.TabPages.Add((i+1).ToString(), "TabPage" + (i+1));
-                        tabControl.TabPages[(i+1).ToString()].Controls.Add(chart);
+                        tabControl.TabPages.Add((i + 1).ToString(), "TabPage" + (i + 1));
+                        tabControl.TabPages[(i + 1).ToString()].Controls.Add(chart);
                     }
+
                     break;
                 case 3:
                     chart = new Chart();
@@ -102,25 +92,33 @@ namespace Postulados_Golomb.Views
                     chart.Series.Add("Postulado3");
                     chart.Series["Postulado3"].ChartType = SeriesChartType.Line;
                     //chart.Series["Postulado3"]["PointWidth"] = "0.5";
-                    chart.Series["Postulado3"].BorderWidth = 3;
-                    chart.Series["Postulado3"].IsValueShownAsLabel = true;
+                    chart.Series["Postulado3"].BorderWidth = 2;
+                    //chart.Series["Postulado3"].IsValueShownAsLabel = true;
                     chart.Dock = DockStyle.Fill;
 
-                    _p3.Select((y, x) => new
-                        {
-                            count = y,
-                            item = x
-                        })
-                        .ToList()
-                        .ForEach(obj =>
-                        {
-                            chart.Series["Postulado3"].Points.AddXY(obj.item, obj.count);
-                        });
+                    chart.Click += Chart_Click;
 
                     tabControl.TabPages.Add("1", "TabPage1");
                     tabControl.TabPages["1"].Controls.Add(chart);
                     break;
             }
+        }
+
+        private void Chart_Click(object sender, EventArgs e)
+        {
+            var chart = sender as Chart;
+
+            _p3.Select((y, x) => new
+                {
+                    count = y,
+                    item = x
+                })
+                .ToList()
+                .ForEach(obj =>
+                {
+                    chart?.Series["Postulado3"].Points.AddXY(obj.item, obj.count);
+                    Application.DoEvents();
+                });
         }
     }
 }
