36d23fa14f9f0f3dee9f1a50dd4fe0fed2998ea1
Oskar <oskarcali18@gmail.com>
2019-11-30 20:47:56 -0600

    Update Element
    Actualizacion de la documentacion

--- Postulados Golomb/Models/Element.cs
+++ Postulados Golomb/Models/Element.cs
@@ -2,13 +2,24 @@
 {
     public class Element
     {
+        /// <summary>
+        ///     Constructor de Element
+        /// </summary>
+        /// <param name="name"></param>
         public Element(string name)
         {
             Name = name;
             Coincidences = 0;
         }
 
+        /// <summary>
+        ///     Propiedad Nombre del Element
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        ///     Propiedad Coincidences del Element
+        /// </summary>
         public int Coincidences { get; set; }
     }
 }
